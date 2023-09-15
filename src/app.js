33f73a6e2a0f98c519836645d14daf9f83f53487
Lordkaito <cespedesisai88@gmail.com>
2023-09-15 21:55:04 +0200

    Restore missing imports from dependencies

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const { setImageRoutes } = require("./routes/imageRoutes");
 const { setUserRoutes } = require("./routes/userRoutes");
