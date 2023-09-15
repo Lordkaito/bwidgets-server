9d192ff7cb6c977f738091a809707f3b8cf7938b
Lordkaito <cespedesisai88@gmail.com>
2023-09-15 21:46:50 +0200

    Format app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const { setRoutes } = require("./routes/imageRoutes");
+const { setImageRoutes } = require("./routes/imageRoutes");
+const { setUserRoutes } = require("./routes/userRoutes");
 const dotenv = require("dotenv");
-const upload = require('./utils/upload'); // Importa la configuración de Multer desde el archivo
-const fs = require('fs');
-
+const upload = require("./utils/upload"); // Importa la configuración de Multer desde el archivo
+const fs = require("fs");
 dotenv.config();
-
 const app = express();
-
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-
-const uploadDir = './uploads';
+const uploadDir = "./uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 // Rutas
-setRoutes(app, upload);
+setImageRoutes(app, upload);
+setUserRoutes(app);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-
-app.use('/uploads', express.static('uploads'));
-
+app.use("/uploads", express.static("uploads"));
 // Puerto
-const port = process.env.PORT;
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
